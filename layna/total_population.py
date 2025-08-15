f51fe43fc897aaeda5b75f77049ba136d5c41d79
laynahong6 <laynahong2025@u.northwestern.edu>
2025-08-15 12:37:45 -0500

    cleaned census data

--- layna/total_population.py
+++ layna/total_population.py
@@ -3,4 +3,17 @@ import requests, json
 from bs4 import BeautifulSoup
 import csv
 
-df = pd.read_csv ('censusdata_ALL.csv')
+df = pd.read_csv ('layna/censusdata_ALL.csv')
+
+illinois = df[df['STNAME'] == 'Illinois']
+
+illinois_counties = illinois[illinois['CTYNAME'] != 'Illinois']
+
+columns_to_keep = ['CTYNAME','ESTIMATESBASE2020','POPESTIMATE2020','POPESTIMATE2021','POPESTIMATE2022','POPESTIMATE2023','POPESTIMATE2024']
+    
+filtered_df = illinois_counties[columns_to_keep]
+
+filtered_df.to_csv ('illinois_census',index=False)
+
+
+
