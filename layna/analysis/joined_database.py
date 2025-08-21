28d36cc2c71ea3197d833c54b3fec3247bfc9b0e
laynahong6 <laynahong2025@u.northwestern.edu>
2025-08-21 14:57:02 -0500

    expressed as a percentage

--- layna/analysis/joined_database.py
+++ layna/analysis/joined_database.py
@@ -5,7 +5,7 @@ df2 = pd.read_csv("layna/csv files/county_population.csv")
 
 merged = pd.merge(df1, df2, on = "County Name", how="inner")
 
-merged["Percentage"] = merged ["2024 Enrollment"] / merged ["2024 Population Estimate"]
+merged["Percentage"] = (merged ["2024 Enrollment"] / merged ["2024 Population Estimate"]) * 100
 
 columns_to_keep = ["County Name", "2024 Enrollment", "2024 Population Estimate", "Percentage"]
 
