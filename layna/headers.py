f5947fcfa24ccb40f8cf434da8bb5445175e5cba
laynahong6 <laynahong2025@u.northwestern.edu>
2025-08-21 10:01:31 -0500

    sort a-z

--- layna/headers.py
+++ layna/headers.py
@@ -10,13 +10,23 @@ col2 = df2.columns[0]
 
 choices = df2[col2].tolist()
 
+# below is getting best case-insenstive match using rapidfuzz library 
+
 def get_best_match(name, choices, score_cutoff=80):
-    lower_choices = {c.lower(): c for c in choices}
-    match = process.extractOne(name.lower(), lower_choices.keys(), scorer=fuzz.ratio, score_cutoff=score_cutoff)
-    return lower_choices[match[0]] if match else name
+    lower_choices = {c.lower(): c for c in choices} # makes a dictionary so lowercase name -> original name
 
-df1[col1] = df1[col1].apply(lambda x: get_best_match(x, choices))
+    match = process.extractOne(
+        name.lower(), # lowercase input version 
+        lower_choices.keys(), # lowercase dictionary keys
+        scorer=fuzz.ratio, 
+        score_cutoff=score_cutoff # ignore bad matches
+        ) 
+    return lower_choices[match[0]] if match else name # in case of match, replace with df2 columns 
 
-df1.rename(columns={col1:col2}, inplace=True)
+df1[col1] = df1[col1].apply(lambda x: get_best_match(x, choices)) # applies function to every county in df1
 
-df1.to_csv('FIXED_county_enrollment.csv', index=False)
+df1.rename(columns={col1:col2}, inplace=True) # renames first column in df1 to that in df2
+
+df1_sorted = df1.sort_values(by="County Name")
+
+df1_sorted.to_csv('FIXED_county_enrollment.csv', index=False)
